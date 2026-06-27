12c95b3493bf7c2fe195a27d47b5d66f14980fc4
Alice <alice@ritsu.dev>
2026-06-27 20:19:50 +1000

    change player subtitle font size

--- src/screens/Player.tsx
+++ src/screens/Player.tsx
@@ -670,7 +670,7 @@ const Player = ({
                 {item.Name}
               </Text>
               {'SeriesName' in item && (
-                <Text style={{ fontSize: 26 }} fontWeight={500}>
+                <Text style={{ fontSize: 16 }} fontWeight={500}>
                   {item.SeriesName +
                     ' ' +
                     (item.ParentIndexNumber === 0
@@ -686,13 +686,13 @@ const Player = ({
               {item.Type === 'MusicVideo' &&
                 'Artists' in item &&
                 item.Artists.length > 0 && (
-                  <Text style={{ fontSize: 20 }} fontWeight={500}>
+                  <Text style={{ fontSize: 16 }} fontWeight={500}>
                     {item.Artists.join(', ')}
                   </Text>
                 )}
             </View>
             <Clock
-              style={{ fontSize: 24, verticalAlign: 'bottom' }}
+              style={{ fontSize: 16, verticalAlign: 'bottom' }}
               fontWeight={500}
             />
           </View>
